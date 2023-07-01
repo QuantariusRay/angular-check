5917cf51a8a66a8651c7d0cb1c84866c40ec0a72
Q <q.ray@jbhunt.com>
2023-06-30 21:19:27 -0500

    Initial commit for Task 5.
    Removing files that aren't needed and updating component file

---
+++ src/app/task5/COMMENTS.md
@@ -0,0 +1,5 @@
+I saw that I missed the delete portion of Task 4. Why is that not in the user story !!!! I guess the notes are the AC? 
+
+Silly...
+
+I'll just add it is part of the final PR for task 5 and it will be available in 4 and 5.
--- src/app/task5/task5.component.ts
+++ src/app/task5/task5.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { TodoFormComponent } from '../components/todo-form/todo-form.component';
+import { TodoListComponent } from '../components/todo-list/todo-list.component';
 
 @Component({
   selector: 'hd-task5',
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './task5.component.html',
-  styleUrls: ['./task5.component.scss']
+  imports: [
+    TodoFormComponent,
+    TodoListComponent
+  ],
+  template: `
+      <h1>Task 5</h1>
+      <app-todo-form [showCategories]="true"/>
+      <app-todo-list [preventEdit]="false" [displayGroupData]="true"/>
+  `,
 })
 export class Task5Component {
 
